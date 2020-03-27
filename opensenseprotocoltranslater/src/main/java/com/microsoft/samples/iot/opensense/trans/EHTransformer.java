b004d6e8f1b3eb0dd53824dd189f86b282979739
Jürgen Mayrbäurl <jmayrbaeurl@live.at>
2020-03-27 13:49:51 +0100

    Added configuration and logging

--- opensenseprotocoltranslater/src/main/java/com/microsoft/samples/iot/opensense/trans/EHTransformer.java
+++ opensenseprotocoltranslater/src/main/java/com/microsoft/samples/iot/opensense/trans/EHTransformer.java
@@ -2,6 +2,8 @@ package com.microsoft.samples.iot.opensense.trans;
 
 import com.microsoft.samples.iot.opensense.dto.SenseBoxValues;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -14,12 +16,16 @@ import org.springframework.messaging.handler.annotation.SendTo;
 @EnableBinding(Processor.class)
 public class EHTransformer {
 
+  private Log logger = LogFactory.getLog(EHTransformer.class);
+
   @Autowired
   private Transformer tansformer;
 
   @StreamListener(Processor.INPUT)
   @SendTo(Processor.OUTPUT)
   public SensorBoxEvent handle(SenseBoxValues boxValues) {
+    if (boxValues != null)
+      logger.info("Translating latest values from box with id " + boxValues.getId());
     return this.tansformer.transform(boxValues);
   }
 }
--- opensensepublisher/src/main/java/com/microsoft/samples/iot/opensense/demo/ScheduledPublisher.java
+++ opensensepublisher/src/main/java/com/microsoft/samples/iot/opensense/demo/ScheduledPublisher.java
@@ -1,6 +1,9 @@
 package com.microsoft.samples.iot.opensense.demo;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,19 +13,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledPublisher {
 
+    private static final Log logger = LogFactory.getLog(ScheduledPublisher.class);
+
     @Autowired
     private EHSender sender;
 
     @Autowired 
     private SenseBoxReader reader;
 
-    private String sensorBoxID = "598c24dae3b1fa001000693d";
+    @Value("${opensensemap.publisher.boxid:598c24dae3b1fa001000693d}")
+    private String sensorBoxID;
 
-    @Scheduled(fixedRate = 5000, initialDelay = 5000)
+    @Scheduled(fixedRateString="${opensensemap.publisher.fixedRate:15000}", initialDelay=5000)
     public void publishLatestValue() {
         
         String latestValue = this.reader.readLatestValuesAsString(this.sensorBoxID);
 
-        this.sender.sendSenseBoxValues(latestValue);
+        if (latestValue != null && latestValue.length() > 0) {
+            this.sender.sendSenseBoxValues(latestValue);
+            logger.info("Latest values of box " + this.sensorBoxID + " sent to Azure");
+        } else
+            logger.error("No values could be read from OpenSenseMap for box with id " + this.sensorBoxID);
     }
 }
