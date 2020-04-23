028288ddd10bfa10d388cf2564165682c5fe86ba
Jürgen Mayrbäurl <jmayrbaeurl@live.at>
2020-04-23 14:03:07 +0200

    Finished v1.0.0 of sensebox-publisher

---
+++ sensorbox-publisher/scripts/runSenseBoxPublisherOnDocker.ps1
@@ -0,0 +1,1 @@
+docker run -d --name sensebox-publisher --env opensensemap.publisher.boxid=5e981cb545f937001cca5734 --env sensorbox.publisher.dps.scope=[Insert your Scope ID] --env sensorbox.publisher.dps.deviceID=[Insert your Registration ID] --env sensorbox.publisher.dps.deviceKey=[Insert your key] jmayrbaeurl/sensebox-publisher
